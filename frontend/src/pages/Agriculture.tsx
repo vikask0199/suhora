68046339583a95ebb214c7390fa170135908cac5
Vikas Kumar <vikasku0199@gmail.com>
2024-02-29 10:42:35 +0530

    last code

--- frontend/src/pages/Agriculture.tsx
+++ frontend/src/pages/Agriculture.tsx
@@ -5,6 +5,8 @@ type agricultureProps = {
 
 
 const Agriculture = ({ currentTheme }: agricultureProps) => {
+    const backgroundColor = currentTheme === 'light'? 'gray.200' : '#282b3c';
+    console.log(backgroundColor)
     return (
         <div>Agriculture</div>
     )
