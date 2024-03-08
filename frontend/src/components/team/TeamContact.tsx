34efedb35e18e9a1d595e88c8419e2c7d94062a3
Lokesh Kumar <lokesh.skshukla@gmail.com>
2024-03-08 12:16:12 +0530

    team and defence page

--- frontend/src/components/team/TeamContact.tsx
+++ frontend/src/components/team/TeamContact.tsx
@@ -3,7 +3,7 @@ import theme from '../../theme'
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { textVarient, textVarientDelayMedium, textVarientSecond } from '../../animation'
-
+import join_team from '../../assets/img/join_team.webp'
 const AnimatedHeading = motion(Heading)
 const AnimatedButton = motion(Button)
 const AnimatedText = motion(Text)
@@ -44,7 +44,8 @@ const TeamContact = () => {
       >
         <Box position="relative" w="full" h="full">
           <Img
-            src="https://images.unsplash.com/photo-1590650153855-d9e808231d41?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
+            // src="https://images.unsplash.com/photo-1590650153855-d9e808231d41?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
+            src={join_team}
             alt="Main Image"
             w="full"
             h="full"
